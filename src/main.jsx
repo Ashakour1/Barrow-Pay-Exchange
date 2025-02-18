366932425016a911649d0013630d0278e7446972
Ashakuur1 <engshakrayare114@gmail.com>
2025-02-18 14:40:27 +0300

    remove React Router setup and render App directly

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const BrowserRouter = createBrowserRouter([
-  {
-    path: "/",
-    Component: App,
-    // children: [
-    //   // {
-    //   //   path: "/login",
-    //   //   Component: Login,
-    //   // },
-    // ],
-  },
-]);
+import { createBrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={BrowserRouter} />
+    <App />
   </React.StrictMode>
 );
