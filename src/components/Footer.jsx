2cbe1f20ebb12fbc2cfcde33fb1113dfd65e1f63
Ashakuur1 <engshakrayare114@gmail.com>
2025-02-21 23:14:01 +0300

    refactor Footer component to use Link from react-router-dom for navigation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaExchangeAlt, FaHome, FaUser } from "react-icons/fa";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Footer = () => {
   const location = useLocation();
@@ -10,8 +10,8 @@ const Footer = () => {
   return (
     <nav className="fixed bottom-0 left-0 right-0 z-10 bg-white dark:bg-gray-800 shadow-lg">
       <div className="flex justify-around items-center h-16 px-6">
-        <a
-          href="/transactions"
+        <Link
+          to="/transactions"
           className={`flex flex-col items-center ${
             isActive("/transactions")
               ? "text-green-600"
@@ -36,9 +36,9 @@ const Footer = () => {
           {!isActive("/transactions") && (
             <span className="text-xs">Transactions</span>
           )}
-        </a>
-        <a
-          href="/"
+        </Link>
+        <Link
+          to="/"
           className={`flex flex-col items-center ${
             isActive("/")
               ? "text-green-600"
@@ -57,9 +57,9 @@ const Footer = () => {
             />
           </div>
           {!isActive("/") && <span className="text-xs">Home</span>}
-        </a>
-        <a
-          href="/profile"
+        </Link>
+        <Link
+          to="/profile"
           className={`flex flex-col items-center ${
             isActive("/profile")
               ? "text-green-600"
@@ -78,7 +78,7 @@ const Footer = () => {
             />
           </div>
           {!isActive("/profile") && <span className="text-xs">Profile</span>}
-        </a>
+        </Link>
       </div>
     </nav>
   );
