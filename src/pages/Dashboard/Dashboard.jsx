aa060d94374ae39df3f00a0b5f6008b32ecd53eb
Ashakuur1 <engshakrayare114@gmail.com>
2025-02-22 18:55:28 +0300

    remove unused Actions component from Dashboard and add comment for WhatsApp Floating Button

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,4 @@
-import Actions from "@/components/Actions";
 import RecentTransactions from "@/components/RecentTransactions";
-import { ArrowDown, ArrowUp, Lock } from "lucide-react";
-import React, { useEffect, useState } from "react";
-import { FaExchangeAlt } from "react-icons/fa";
 import WalletBalance from "./WalletBalance";
 
 const Dashboard = () => {
@@ -10,11 +6,11 @@ const Dashboard = () => {
     <main className="px-4 pt-20 pb-24">
       {/* Wallet Balance */}
       <WalletBalance />
-      
-      <Actions />
 
       {/* Recent Transactions */}
       <RecentTransactions />
+
+      {/* WhatsApp Floating Button */}
     </main>
   );
 };
